a6c668a95ea8f9b1a8ff4d5b5ec833d633dd9b64
Lex Benjamin <abessilfie@st.ug.edu.gh>
2024-01-06 19:47:19 +0000

    Dependencies and app update

---
+++ app/recoverpassword/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecoverPassword = () => {
+  return (
+    <div className=" flex justify-center items-center flex-col h-screen w-screen">
+      RecoverPassword
+    </div>
+  );
+};
+
+export default RecoverPassword;
