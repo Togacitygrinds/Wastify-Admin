1a4afdcaa6e19209ae6d125d3608b50ef022df5b
Lex Benjamin <abessilfie@st.ug.edu.gh>
2024-01-08 16:42:15 +0000

    Updated the Tracker util

--- app/utils/Tracker.js
+++ app/utils/Tracker.js
@@ -1,130 +1,62 @@
-import Axios from './Axios';
-import { toast } from 'react-toastify';
+import Axios from "./Axios";
+import { toast } from "react-toastify";
 
-// Create a new tracker
-export const createTracker = async (trackerData, setLoading, callback) => {
+export const createTracker = async (tracking_details, setLoading, callback) => {
   try {
     const { data } = await Axios({
-      method: 'POST',
-      url: '/tracker/create',
-      data: trackerData,
+      method: "POST",
+      url: `/track/new`,
+      data: tracking_details,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
+
     if (data.success) {
+      toast.success("Initated a new tracker");
       callback(data);
-      toast.success('Tracker created successfully');
     } else {
       setLoading(false);
-      toast.error("Sorry we couldn't create the tracker. Please try again");
+      toast.error(`Failed to create tracker: ${data.message}`);
     }
+
+    return data;
   } catch (error) {
-    setLoading(false);
-    toast.error(
-      error?.response?.data?.message || "Just a waste of time! Couldn't create the tracker"
-    );
+    console.error("Error creating tracker:", error);
+    toast.error("Failed to create tracker");
+    return {
+      success: false,
+      message: "Failed to create tracker",
+      error,
+    };
   }
 };
 
-// Fetch Trackers
-export const fetchTrackers = async (setLoading, callback) => {
+export const updateLiveLocation = async (userId, coordinates) => {
   try {
     const { data } = await Axios({
-      method: 'GET',
-      url: '/tracker/all',
+      method: "POST",
+      url: `/track/`,
+      data: { userId, coordinates },
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
-    if (data.success) {
-      callback(data);
-    } else {
-      setLoading(false);
-      toast.error("Sorry we couldn't fetch trackers. Please try again");
-    }
-  } catch (error) {
-    setLoading(false);
-    toast.error(
-      error?.response?.data?.message || "Just a waste of time! Couldn't fetch trackers"
-    );
-  }
-};
 
-// Delete a tracker
-export const deleteTracker = async (id, setLoading, callback) => {
-  setLoading(true);
-  try {
-    const { data } = await Axios({
-      method: 'DELETE',
-      url: `/tracker/delete/${id}`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
     if (data.success) {
-      callback(data);
-      toast.success('Tracker deleted successfully');
+      toast.success(data.message);
     } else {
-      setLoading(false);
-      toast.error("Sorry we couldn't delete the tracker. Please try again");
+      toast.error(`Failed to update live location: ${data.message}`);
     }
-  } catch (error) {
-    setLoading(false);
-    toast.error(
-      error?.response?.data?.message || "Just a waste of time! Couldn't delete the tracker"
-    );
-  }
-};
 
-// Update a tracker
-export const updateTracker = async (trackerData, setLoading, callback) => {
-  setLoading(true);
-  try {
-    const { data } = await Axios({
-      method: 'PUT',
-      url: `/tracker/update/${trackerData._id}`,
-      data: trackerData,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (data.success) {
-      callback(data);
-      toast.success('Tracker updated successfully');
-    } else {
-      setLoading(false);
-      toast.error("Sorry we couldn't update the tracker. Please try again");
-    }
+    return data;
   } catch (error) {
-    setLoading(false);
-    toast.error(
-      error?.response?.data?.message || "Just a waste of time! Couldn't update the tracker"
-    );
-  }
-};
-
-// Fetch a Tracker
-export const fetchTracker = async (id, setLoading, callback) => {
-  setLoading(true);
-  try {
-    const { data } = await Axios({
-      method: 'GET',
-      url: `/tracker/${id}`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (data.success) {
-      callback(data);
-    } else {
-      setLoading(false);
-      toast.error("Sorry we couldn't fetch the requested tracker at this time.");
-    }
-  } catch (error) {
-    setLoading(false);
-    toast.error(
-      error?.response?.data?.message || "Just a waste of time! Couldn't fetch the tracker"
-    );
+    console.error("Error updating live location:", error);
+    toast.error("Failed to update live location");
+    return {
+      success: false,
+      message: "Failed to update live location",
+      error,
+    };
   }
 };
