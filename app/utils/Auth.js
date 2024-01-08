c020584e026a55964be6055058dc08e3ad8f0e6c
Lex Benjamin <abessilfie@st.ug.edu.gh>
2024-01-08 13:19:31 +0000

    Utils update

--- app/utils/Auth.js
+++ app/utils/Auth.js
@@ -1,1 +1,12 @@
 import { toast } from "react-toastify";
+import { Axios } from "axios";
+
+export const UserSignUp = async (info, setLoading, callback) => {
+    try {
+        
+        
+    } catch (error) {
+        setLoading(false)
+        toast.error(error?.response?.data?.message || "Uh oh! User registration failed.")
+    }
+}
---
+++ app/utils/Functions.js
@@ -0,0 +1,10 @@
+import {v4 as uuidv4} from 'uuid';
+
+export const generateRandomId = () => {
+  return uuidv4().slice(0, 8);
+}
+
+export const generateInitial = (name) => {
+    const userInitial = name.charAt(0);
+    return userInitial.toUpperCase() 
+}
