e868dec7bcdcfacbf69665662809bf8fcf3aaf9d
Lex Benjamin <abessilfie@st.ug.edu.gh>
2024-01-08 13:54:07 +0000

    Updated the Authentication in utils

--- app/utils/Auth.js
+++ app/utils/Auth.js
@@ -1,12 +1,112 @@
 import { toast } from "react-toastify";
 import { Axios } from "axios";
 
+// Create a new user
 export const UserSignUp = async (info, setLoading, callback) => {
-    try {
-        
-        
-    } catch (error) {
-        setLoading(false)
-        toast.error(error?.response?.data?.message || "Uh oh! User registration failed.")
+  try {
+    const { data } = await Axios({
+      method: "post",
+      url: "/auth/signup",
+      data: info,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (data.success) {
+      callback(data);
+    } else {
+      setLoading(false);
     }
-}
+  } catch (error) {
+    setLoading(false);
+    toast.error(
+      error?.response?.data?.message || "Uh oh! User registration failed."
+    );
+  }
+};
+
+// Login a user
+export const UserLogin = async (info, setLoading, callback) => {
+  try {
+    const { data } = await Axios({
+      method: "post",
+      url: "/auth/login",
+      data: info,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (data.success) {
+      callback(data);
+    } else {
+      setLoading(false);
+    }
+  } catch (error) {
+    setLoading(false);
+    toast.error(error?.response?.data?.message || "Uh oh! Login failed.");
+  }
+};
+
+// Refresh Token
+export const RefreshToken = async (callback) => {
+  try {
+    const { data } = await Axios({
+      method: "post",
+      url: "/auth/refresh-token",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (data.success) {
+      callback(data);
+    }
+  } catch (error) {
+    toast.error(
+      error?.response?.data?.message || "Uh oh! Session token refresh failed."
+    );
+  }
+};
+
+// Admin Login
+export const AdminLogin = async (info, setLoading, callback) => {
+  try {
+    const { data } = await Axios({
+      method: "post",
+      url: "/admin/login",
+      data: info,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (data.success) {
+      callback(data);
+    } else {
+      setLoading(false);
+    }
+  } catch (error) {
+    setLoading(false);
+    toast.error(error?.response?.data?.message || "Uh oh! Login failed.");
+  }
+};
+
+// Admin Signup
+export const AdminSignUp = async (info, setLoading, callback) => {
+    try {
+      const { data } = await Axios({
+        method: "post",
+        url: "/admin/register",
+        data: info,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (data.success) {
+        callback(data);
+      } else {
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error(error?.response?.data?.message || "Uh oh! Admin sign up failed.");
+    }
+  };
